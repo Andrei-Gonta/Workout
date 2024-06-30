97fed35cc0aaaeaf6c632085e12ef49a6bb8c456
Andrei Gonta <gontaandrei02@gmail.com>
2024-06-30 20:28:42 +0300

    final commit first week (need to be fixed)

--- Data_Base_Access/Migrations/20240630170910_secondtry.Designer.cs
+++ Data_Base_Access/Migrations/20240630170910_secondtry.Designer.cs
@@ -4,6 +4,7 @@ using Data_Base_Access;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data_Base_Access.Migrations
 {
     [DbContext(typeof(WorkoutContext))]
-    partial class WorkoutContextModelSnapshot : ModelSnapshot
+    [Migration("20240630170910_secondtry")]
+    partial class secondtry
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data_Base_Access/Migrations/20240630170910_secondtry.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_Base_Access.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondtry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "duration",
+                table: "Workout",
+                newName: "Duration");
+
+            migrationBuilder.RenameColumn(
+                name: "date",
+                table: "Workout",
+                newName: "Date");
+
+            migrationBuilder.RenameColumn(
+                name: "weight",
+                table: "User",
+                newName: "Weight");
+
+            migrationBuilder.RenameColumn(
+                name: "surname",
+                table: "User",
+                newName: "Surname");
+
+            migrationBuilder.RenameColumn(
+                name: "phone",
+                table: "User",
+                newName: "Phone");
+
+            migrationBuilder.RenameColumn(
+                name: "name",
+                table: "User",
+                newName: "Name");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "User",
+                newName: "Email");
+
+            migrationBuilder.RenameColumn(
+                name: "age",
+                table: "User",
+                newName: "Age");
+
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "User",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Weight",
+                table: "User",
+                type: "float",
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Phone",
+                table: "User",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Age",
+                table: "User",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateTable(
+                name: "Exercises",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ex_Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Exercises", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ExercisesLog",
+                columns: table => new
+                {
+                    Id_Workout = table.Column<int>(type: "int", nullable: false),
+                    Id_Exercice = table.Column<int>(type: "int", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: true),
+                    Sets = table.Column<int>(type: "int", nullable: true),
+                    Reps = table.Column<int>(type: "int", nullable: true),
+                    Time_Session = table.Column<double>(type: "float", nullable: true),
+                    Distance = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExercisesLog", x => new { x.Id_Workout, x.Id_Exercice });
+                    table.ForeignKey(
+                        name: "FK_ExercisesLog_Exercises_Id_Exercice",
+                        column: x => x.Id_Exercice,
+                        principalTable: "Exercises",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ExercisesLog_Workout_Id_Workout",
+                        column: x => x.Id_Workout,
+                        principalTable: "Workout",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExercisesLog_Id_Exercice",
+                table: "ExercisesLog",
+                column: "Id_Exercice");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExercisesLog");
+
+            migrationBuilder.DropTable(
+                name: "Exercises");
+
+            migrationBuilder.RenameColumn(
+                name: "Duration",
+                table: "Workout",
+                newName: "duration");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Workout",
+                newName: "date");
+
+            migrationBuilder.RenameColumn(
+                name: "Weight",
+                table: "User",
+                newName: "weight");
+
+            migrationBuilder.RenameColumn(
+                name: "Surname",
+                table: "User",
+                newName: "surname");
+
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "User",
+                newName: "phone");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "User",
+                newName: "name");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "User",
+                newName: "email");
+
+            migrationBuilder.RenameColumn(
+                name: "Age",
+                table: "User",
+                newName: "age");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "User",
+                newName: "id");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "weight",
+                table: "User",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0,
+                oldClrType: typeof(double),
+                oldType: "float",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phone",
+                table: "User",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "age",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
